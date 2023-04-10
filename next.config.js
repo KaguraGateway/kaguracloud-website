6d9b3127c87004cbf417a063d7b967c1972d3407
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-04-10 17:11:36 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   experimental: {
     runtime: 'experimental-edge',
   },
-  swcMinify: true,
   images: {
     unoptimized: true,
   },
