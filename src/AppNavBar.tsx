ece49e036c9bb5e4853dc211bab291d520b42daa
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-04-10 16:56:29 +0900

    update

--- src/AppNavBar.tsx
+++ src/AppNavBar.tsx
@@ -1,23 +1,14 @@
 import { AppBar, Box, Container, createStyles, Toolbar, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import Image from 'next/image';
 import logoPng from '../public/assets/images/logo.png';
 
-const useStyles = makeStyles(() => ({
-    logoImage: {
-        display: "inline-block",
-    }
-}));
-
 export default function AppNavBar() {
-    const classes = useStyles();
-
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <a href="https://kagura.cloud">
-                        <Image className={classes.logoImage} src={logoPng} alt="Kagura Cloud" width="100%" height="56px" />
+                        <Image src={logoPng} alt="Kagura Cloud" />
                     </a>
                 </Toolbar>
             </Container>
